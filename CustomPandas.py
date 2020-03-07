2938c653fa0deca0e458aaef8bdb795855fa9307
James Kabbes <james.kabbes@gmail.com>
2020-03-07 17:04:48 -0600

    added mismatch on time series func

--- CustomPandas.py
+++ CustomPandas.py
@@ -257,6 +257,129 @@ def rename_cols(df, existing, new):
 
     return df
 
+
+def adjust_mismatched_time_series(df_to_keep, df_to_adjust, df1_dt_col, df2_dt_col, adjust_for_all_entries = True, how = '>='):
+
+    df_to_keep = sort_df(df_to_keep, df1_dt_col)
+    df_to_adjust = sort_df(df_to_adjust, df2_dt_col)
+
+    og_datetimes = list(df_to_keep[df1_dt_col])
+
+    print (df_to_keep)
+    print (df_to_adjust)
+
+    adjusted_cols = list(df_to_adjust.columns)
+    dt_ind = adjusted_cols.index( df2_dt_col )
+    dt2_new_dt_col = 'Sampled' + params.split + df2_dt_col
+    adjusted_cols.insert(0, dt2_new_dt_col)
+
+    rows = np.array([ [0,] * len(adjusted_cols) ] )
+    other_ind = 0
+
+    for og_dt_num in range(len(og_datetimes)):
+
+        og_dt = og_datetimes[og_dt_num]
+        try:
+            og_dt2 = og_datetimes[og_dt_num + 1]
+        except:
+            og_dt2 = None
+
+        print_progress(og_dt_num+1, len(og_datetimes))
+
+        for i in range(other_ind, len(df_to_adjust)):
+            add = False
+            br = False
+            dt = df_to_adjust.loc[i, df2_dt_col]
+
+            try:
+                dt2 = df_to_adjust.loc[i+1, df2_dt_col]
+            except:
+                dt2 = None
+
+            ###Add dt to the list if og_dt is >= to the dt value
+            if how == '>=':
+                if og_dt < dt:
+                    if i == 0:
+                        if adjust_for_all_entries:
+                            add = True
+                            br = True
+
+                else: #og_dt >= dt
+                    br = True
+                    add = True
+
+                    if dt2 != None:
+                        if og_dt >= dt2:
+                            other_ind += 1
+                            continue
+
+            elif how == '>':
+                if og_dt <= dt:
+                    if i == 0:
+                        if adjust_for_all_entries:
+                            add = True
+                            br = True
+
+                else: #og_dt >= dt
+                    br = True
+                    add = True
+
+                    if dt2 != None:
+                        if og_dt > dt2:
+                            other_ind += 1
+                            continue
+
+            elif how == '<=':
+
+                if og_dt > dt:
+                    if i == len(df_to_adjust) - 1:
+                        if adjust_for_all_entries:
+                            add = True
+                            br = True
+                    else:
+                        other_ind += 1
+
+                else: #og_dt >= dt
+                    br = True
+                    add = True
+
+
+            elif how == '<':
+
+                if og_dt >= dt:
+                    if i == len(df_to_adjust) - 1:
+                        if adjust_for_all_entries:
+                            add = True
+                            br = True
+                    else:
+                        other_ind += 1
+
+                else: #og_dt > dt
+                    br = True
+                    add = True
+
+
+            if add:
+                row = list(df_to_adjust.loc[i])
+                row.insert(0, og_dt)
+                rows = np.append(rows, [ row ], axis = 0)
+
+            if br:
+                break
+
+    #remove the first row
+    rows =  rows[1: ,:]
+    print (rows)
+    print (adjusted_cols)
+
+    new_dict = {}
+    for i in range(len(adjusted_cols)):
+        new_dict[ adjusted_cols[i] ] = list(rows[:,i])
+
+    adjusted_df = pd.DataFrame(new_dict)
+    return adjusted_df
+
+
 def multiple_filters(df, columns, two_dim_list):
 
     '''columns = ['Age','Name']
